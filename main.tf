94b66b85bbdd429a60e897a5ef0f99b163ba5c23
Yash Rawal <yashrawal96@gmail.com>
2024-06-24 21:21:06 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+# # resource "<resource_type>" "<resource_reference_name>"
+resource "aws_s3_bucket" "my-first-bucket" {
+  # resource arguments
+  bucket = "yash-25121995"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my-second-bucket" {
+  bucket = "yash-25121"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my-third-bucket" {
+  bucket = "yash-251219951234"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "QA"
+  }
+}
+
+# recover state file 
+
+
+
+
+
